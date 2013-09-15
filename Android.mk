5eefde1cd8c185a23583ff0263bdbe5ec9faaea3
Patrick Harbers <jgrharbers@gmail.com>
2013-09-16 00:49:18 +0200

    Add libenc (Apache-harmony(intel)) for building. It doesnt have a pointer to it to build it.

--- Android.mk
+++ Android.mk
@@ -3,5 +3,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_BOARD_PLATFORM),sc1)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+# include x86 encoder (apache-harmony (intel))
+include $(TOP)/dalvik/vm/compiler/codegen/x86/libenc/Android.mk
 endif
 
