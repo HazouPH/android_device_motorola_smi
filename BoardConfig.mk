b675e5472d014ecc16ac687e80c3d1e426505845
Patrick Harbers <jgrharbers@gmail.com>
2013-12-02 17:23:40 +0100

    Fixed internal and external mounting, cleaned up the code a little
    Change-Id: I5f5f4a7034ca3fb0f043ff3fcd27ed300917635b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,6 +2,10 @@ include $(GENERIC_X86_CONFIG_MK)
 
 LOCAL_PATH := device/motorola/smi
 
+# Make settings
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
 # Board configuration
 TARGET_BOOTLOADER_BOARD_NAME := smi
 TARGET_CPU_ABI := x86
@@ -60,12 +64,9 @@ TARGET_GLOBAL_LDFLAGS += -Wl,-O1
 # Connectivity - Wi-Fi
 USES_TI_MAC80211 := true
 ifdef USES_TI_MAC80211
-WPA_SUPPLICANT_VERSION := 		VER_0_8_X_TI
+WPA_SUPPLICANT_VERSION := 		VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := 		NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := 	lib_driver_cmd_wl12xx
-BOARD_HOSTAPD_DRIVER := 		NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := 		lib_driver_cmd_wl12xx
-PRODUCT_WIRELESS_TOOLS := 		true
 BOARD_WLAN_DEVICE := 			wl12xx_mac80211
 BOARD_SOFTAP_DEVICE := 			wl12xx_mac80211
 WIFI_DRIVER_MODULE_PATH := 		"/system/lib/modules/wl12xx_sdio.ko"
@@ -74,18 +75,25 @@ WIFI_FIRMWARE_LOADER := 		""
 COMMON_GLOBAL_CFLAGS += 		-DUSES_TI_MAC80211
 endif
 
+TARGET_MODULES_SOURCE := "hardware/ti/wlan/mac80211/compat_wl12xx"
+
+WIFI_MODULES:
+	make -C $(TARGET_MODULES_SOURCE) KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
+
+TARGET_KERNEL_MODULES := WIFI_MODULES
+
 # Global flags
 COMMON_GLOBAL_CFLAGS += -DMOTOROLA_UIDS
 
 TARGET_USES_MOTOROLA_LOG := true
 
-# Make settings
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
 # Use boot tools to make Intel-formatted images
-USE_PREBUILT_RAMDISK := true
-DEVICE_RAMDISK_CONTENT := $(LOCAL_PATH)/ramdisk/prebuilt-ramdisk
+# USE_PREBUILT_RAMDISK := true
 DEVICE_BASE_BOOT_IMAGE := $(LOCAL_PATH)/blobs/boot.image
 DEVICE_BASE_RECOVERY_IMAGE := $(LOCAL_PATH)/blobs/recovery.image
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/boottools/boot.mk
@@ -94,25 +102,17 @@ BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/boottools/boot.mk
 BOARD_KERNEL_BASE := 0x1200000
 BOARD_KERNEL_BASE := 0x000400
 BOARD_KERNEL_PAGESIZE := 4096
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/blobs/kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/blobs/bzImage
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := i686-linux-android-4.6
 TARGET_KERNEL_CONFIG := i386_mfld_oxavelar_defconfig
-#TARGET_KERNEL_SOURCE := kernel/kernel/linux-3.0
-KERNEL_MODULES_IN_ROOT := true
-BOARD_KERNEL_CMDLINE := init=/init pci=noearly console=logk0 vmalloc=272M earlyprintk=nologger hsu_dma=7 kmemleak=off androidboot.bootmedia=sdcard androidboot.hardware=sc1 emmc_ipanic.ipanic_part_number=6 loglevel=4
+TARGET_KERNEL_SOURCE := linux-3.0
+KERNEL_MODULES_IN_ROOT := false
+BOARD_KERNEL_CMDLINE := init=/init pci=noearly console=logk0 vmalloc=256M earlyprintk=nologger hsu_dma=7 kmemleak=off androidboot.bootmedia=sdcard androidboot.hardware=sc1 emmc_ipanic.ipanic_part_number=6 loglevel=4
 
-WLAN_MODULES:
-	make clean -C hardware/ti/wlan/mac80211/compat_wl12xx
-	make -j8 -C hardware/ti/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=i386
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
-
-#TARGET_KERNEL_MODULES += WLAN_MODULES
-
-# Partition information
+# Storage information
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+BOARD_VOLD_MAX_PARTITIONS := 32
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 11534336 # 0x00b00000
@@ -120,7 +120,9 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912 # 0x20000000
 BOARD_FLASH_BLOCK_SIZE := 2048
 
 # Ramdisk
-# TARGET_PROVIDES_INIT_RC := true
+USE_PREBUILT_RAMDISK := true
+DEVICE_RAMDISK_CONTENT := $(LOCAL_PATH)/ramdisk/prebuilt-ramdisk
+TARGET_PROVIDES_INIT_RC := true
 
 # Customize the malloced address to be 16-byte aligned
 BOARD_MALLOC_ALIGNMENT := 16
@@ -133,14 +135,8 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_NFC := true
 
 # Audio
-BOARD_USES_ALSA_AUDIO := true
-BOARD_USES_FLUENCE_INCALL := true
-BOARD_USES_SEPERATED_AUDIO_INPUT := true
-BOARD_USES_MOTOROLA_EMU_AUDIO := true
-
-# Security
-# BUILD_WITH_CHAABI_SUPPORT := true
-BUILD_WITH_WATCHDOG_DAEMON_SUPPORT := true
+#BOARD_USES_ALSA_AUDIO := false
+COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
 
 # Graphics
 USE_OPENGL_RENDERER := true
@@ -157,6 +153,9 @@ TARGET_FORCE_CPU_UPLOAD := true
 
 # Turn on GR_STATIC_RECT_VB flag in skia to boost performance
 TARGET_USE_GR_STATIC_RECT_VB := true
+
+# Skip droiddoc build to save build time
+BOARD_SKIP_ANDROID_DOC_BUILD := true
 
 # Recovery configuration global
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
