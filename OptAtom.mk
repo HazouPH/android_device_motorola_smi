be62fbc52b0e52084c36efaed62db7314a638a2d
HazouPH <jgrharbers@gmail.com>
2014-11-21 11:39:37 +0100

    Fix houdini support
    Installing an apk works in the following way through play store. First play store checks the abi and abi2 in build.prop. If the apk is available it checks again while installing the apk if the apk is for a specific arch. First the abi(x86), then the abi2(armeabi-v7a, the fastest one) and if even that one doesnt work the upgradeabi(armeabi, a little slower). 
    
    Most apk's will work with the first and second, but if it doesn't, fall back to the third. Houdini supports armeabi, armeabi-v7a, armeabi-v7a neon.
    
    Still testing and I maybe incorrect

--- OptAtom.mk
+++ OptAtom.mk
@@ -59,6 +59,6 @@ BOARD_MALLOC_ALIGNMENT := 16
 
 ifdef INTEL_HOUDINI
 ADDITIONAL_BUILD_PROPERTIES += ro.product.cpu.abi2=armeabi-v7a
-ADDITIONAL_BUILD_PROPERTIES += ro.product.cpu.upgradeabi=armeabi-v7a
+ADDITIONAL_BUILD_PROPERTIES += ro.product.cpu.upgradeabi=armeabi
 ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.houdini=on
 endif
