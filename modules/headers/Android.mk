06612c4d04aa1c12b10476ddb8cfb9366188b9bd
HazouPH <jgrharbers@gmail.com>
2014-11-30 20:20:55 +0100

    Add shared databuffer for Intel
    Change-Id: I30867973c9305ef119bb45b4e8a829b4b3b3d51a

--- modules/headers/Android.mk
+++ modules/headers/Android.mk
@@ -5,3 +5,9 @@ LOCAL_COPY_HEADERS := \
     hal_public.h
 LOCAL_COPY_HEADERS_TO := pvr/hal
 include $(BUILD_COPY_HEADERS)
+
+include $(CLEAR_VARS)
+LOCAL_COPY_HEADERS := \
+    IntelMetadataBuffer.h
+LOCAL_COPY_HEADERS_TO := libmix_videoencoder
+include $(BUILD_COPY_HEADERS)
