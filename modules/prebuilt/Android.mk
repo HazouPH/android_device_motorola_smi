268db908f6155c535bc1580edbb9e9250bbb09f2
HazouPH <jgrharbers@gmail.com>
2014-11-30 20:22:35 +0100

    Add intel prebuilt library
    Change the way prebuilt libs are included
    
    Change-Id: Ia8d28c2b2a2cbbb1c3abd485cd23918b533eb72b

--- modules/prebuilt/Android.mk
+++ modules/prebuilt/Android.mk
@@ -1,8 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
+PREBUILT_DIR := vendor/motorola/smi/proprietary
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtinyalsa
-LOCAL_SRC_FILES := libtinyalsa.so
+LOCAL_SRC_FILES := $(PREBUILT_DIR)/lib/libtinyalsa.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libintelmetadatabuffer
+LOCAL_SRC_FILES := $(PREBUILT_DIR)/lib/libintelmetadatabuffer.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
