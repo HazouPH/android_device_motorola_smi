02b756ac83da08b2fc7ce8ddd2a393124569034d
HazouPH <jgrharbers@gmail.com>
2014-12-17 14:22:31 +0100

    Update fix-mac.sh

--- modules/prebuilt/bin/fix-mac.sh
+++ modules/prebuilt/bin/fix-mac.sh
@@ -3,7 +3,7 @@
 PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
 ORIG_NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs_128x.bin
 NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
-PDS_NVS_BIN=/pds/wifi/wl1271-nvs.bin
+PDS_NVS_BIN=/pds/wifi/nvs_map_mac80211.bin
 
 if busybox [ ! -f "$NVS_BIN" ]; then
     toolbox mount -o remount,rw /system
