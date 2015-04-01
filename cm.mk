57802554a7c55fc2d0ef5a635cefc49c8e5bd929
HazouPH <jgrharbers@gmail.com>
2015-04-01 22:11:35 +0200

    Use 480p instead of half-res-540p as bootanimation
    Our gpu runs out of memory when using the full 540p version. With the
    480 it doesn't and we just mis 60p instead of 270p in sharpness and
    pixel usage.
    
    Change-Id: I0b2a29ab9b0b21b5ff5fd8ed5ba95659ee68f5e9

--- cm.mk
+++ cm.mk
@@ -5,9 +5,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Boot animation
-TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_WIDTH := 480	#Real=540 (Fix bootanimation)
 
 # Release name
 PRODUCT_RELEASE_NAME := Razr I
