76a7d035599df4e4548e74ea3564f151abad4719
HazouPH <jgrharbers@gmail.com>
2015-10-25 22:11:57 +0100

    Use libenc from device tree
    Change-Id: I704b9e495f76308e3d5725ce3a9d1375c1e2a0d2

--- Android.mk
+++ Android.mk
@@ -3,8 +3,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_BOARD_PLATFORM),sc1)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-# include x86 encoder (apache-harmony (intel))
-include dalvik/vm/compiler/codegen/x86/libenc/Android.mk
 ifeq ($(wildcard ../../../patches/Android.mk),)
 $(info Motorola Razr I patch directory exist, hopefully applied)
 include patches/Android.mk
