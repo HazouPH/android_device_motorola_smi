9e0960f5522e4926078e95e7f47550f20ca40b2a
Patrick Harbers <jgrharbers@gmail.com>
2016-04-01 15:07:30 +0200

    Remove wpa and hostapd private lib define
    Change-Id: I0ac2d9657b89ab405f1c2b5f4a7ccad6bc77c95f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,11 +22,9 @@ TARGET_CPU_SMP := true
 # Connectivity - Wi-Fi
 USES_TI_MAC80211                 := true
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx-compat
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_HOSTAPD_DRIVER             := NL80211
 COMMON_GLOBAL_CFLAGS             += -DUSES_TI_MAC80211
 
 TARGET_MODULES_SOURCE := "hardware/ti/wlan-intel/wl12xx-compat"
