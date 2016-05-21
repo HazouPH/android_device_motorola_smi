f531a8b568a7e4e95cfb7dc16e221471f3a5bc7c
Patrick Harbers <jgrharbers@gmail.com>
2016-05-21 22:49:16 +0200

    Fix permissions for mediaserver and lots of others
    Change-Id: I80391569fb733aa67451c1dc5182baf3a447dea9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ BOARD_SYSTEM_FILESYSTEM                 := ext4
 
 # Ramdisk
 TARGET_PROVIDES_INIT_RC := true
-NR_SVC_SUPP_GIDS := 28
+TARGET_NR_SVC_SUPP_GIDS := 28
 
 # Blutetooth
 BOARD_HAVE_BLUETOOTH := true
