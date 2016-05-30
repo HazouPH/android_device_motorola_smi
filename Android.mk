d47a332bd9c277230be28b9066a3f59152dfcb26
Patrick Harbers <jgrharbers@gmail.com>
2016-05-30 21:26:03 +0200

    Update Android.mk
    My own symlinks

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_BOARD_PLATFORM),sc1)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
-
+#include $(call all-subdir-makefiles,$(LOCAL_PATH))
 # create symlink compiled source directory in root folder
+$(shell rm compiled-source)
 $(shell ln -sf out/target/product/smi/ compiled-source)
+endif
