5a81b8edd6465c5128e043eff50bd39af9959dfc
Patrick Harbers <jgrharbers@gmail.com>
2016-06-08 23:30:52 +0200

    Use target_device instead of device_name

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(PRODUCT_DEVICE),smi)
+ifeq ($(TARGET_DEVICE),smi)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-#include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
 # create symlink compiled source directory in root folder
 $(shell rm compiled-source)
 $(shell ln -sf out/target/product/$(PRODUCT_DEVICE)/ compiled-source)
