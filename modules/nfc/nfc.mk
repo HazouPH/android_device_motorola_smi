50b1a5308cdb4cc0fc1f0adba7ccd8bdecf384f0
Patrick Harbers <jgrharbers@gmail.com>
2016-06-16 01:12:45 +0200

    Let NFC work, disabling some non-working things
    Should they work?

--- modules/nfc/nfc.mk
+++ modules/nfc/nfc.mk
@@ -5,9 +5,7 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/etc/nfcee_access.xml:system/etc/nfc
 
 PRODUCT_COPY_FILES += \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 # NFC Support
 PRODUCT_PACKAGES += \
