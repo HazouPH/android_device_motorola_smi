d3bd773b2bf17c4026806e0e5258e954cb4a3274
Patrick Harbers <jgrharbers@gmail.com>
2016-06-16 15:46:34 +0200

    Revert "Build tinyalsa from source"
    This reverts commit eada4f83f0e2918f7ea6f264fc604bcb7e34900c.

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -2,13 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 VENDOR_PREBUILT_DIR := ../../../../vendor/motorola/smi/proprietary
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := libtinyalsa
-#LOCAL_SRC_FILES := $(VENDOR_PREBUILT_DIR)/lib/libtinyalsa.so
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-#LOCAL_MODULE_SUFFIX := .so
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtinyalsa
+LOCAL_SRC_FILES := $(VENDOR_PREBUILT_DIR)/lib/libtinyalsa.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libpvr2d
