1f57b0a1bbf2d54c3d078a442f049e43fe778a7d
Patrick Harbers <jgrharbers@gmail.com>
2016-09-20 15:25:29 +0200

    Add filesystem configs
    Also remove houdini permission patch

--- device.mk
+++ device.mk
@@ -225,6 +225,10 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.adb.secure=0 \
     ro.debuggable=1
 
+# FS config
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # SGX540 is slower with the scissor optimization enabled
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.disable_scissor_opt=true
