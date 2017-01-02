e3390bf9cc47c4e560e512aeada180e5b8ca392c
Patrick Harbers <jgrharbers@gmail.com>
2017-01-02 22:11:20 +0100

    Update prebuilt/Android.mk
    Change-Id: Ia25570f98fe598ff40acf92c98f20a5dfa4c0a81

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -4,7 +4,7 @@ VENDOR_PREBUILT_DIR := ../../../../vendor/motorola/smi/proprietary
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmixvbp
-LOCAL_SRC_FILES := $(VENDOR_PREBUILT_DIR)/lib/libmixvbp.so
+LOCAL_SRC_FILES := lib/libmixvbp.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -12,7 +12,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmixvbp_h264
-LOCAL_SRC_FILES := $(VENDOR_PREBUILT_DIR)/lib/libmixvbp_h264.so
+LOCAL_SRC_FILES := lib/libmixvbp_h264.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -20,7 +20,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmixvbp_mpeg4
-LOCAL_SRC_FILES := $(VENDOR_PREBUILT_DIR)/lib/libmixvbp_mpeg4.so
+LOCAL_SRC_FILES := lib/libmixvbp_mpeg4.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -28,7 +28,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmixvbp_vc1
-LOCAL_SRC_FILES := $(VENDOR_PREBUILT_DIR)/lib/libmixvbp_vc1.so
+LOCAL_SRC_FILES := lib/libmixvbp_vc1.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -44,20 +44,16 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := msvdx_fw_mfld_DE2.0.bin
-LOCAL_MODULE_OWNER := intel
+LOCAL_SRC_FILES := etc/firmware/msvdx_fw_mfld_DE2.0.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := etc/firmware/msvdx_fw_mfld_DE2.0.bin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := topazsc_fw.bin
-LOCAL_MODULE_OWNER := intel
+LOCAL_SRC_FILES := etc/firmware/topazsc_fw.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := etc/firmware/topazsc_fw.bin
 include $(BUILD_PREBUILT)
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
