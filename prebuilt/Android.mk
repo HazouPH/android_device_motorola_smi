9b2f4e52feeedfe871ef17c3f96124aab1d8bac1
Patrick Harbers <jgrharbers@gmail.com>
2017-01-04 23:58:54 +0100

    Fixup encoding. Took way to lon....
    Change-Id: I295319670f91e369e39ad654592d9b294080c1b6

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -55,6 +55,7 @@ LOCAL_MODULE := msvdx_fw_mfld_DE2.0.bin
 LOCAL_SRC_FILES := etc/firmware/msvdx_fw_mfld_DE2.0.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -62,6 +63,7 @@ LOCAL_MODULE := topazsc_fw.bin
 LOCAL_SRC_FILES := etc/firmware/topazsc_fw.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 include $(BUILD_PREBUILT)
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
