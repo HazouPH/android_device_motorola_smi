3ef130df29479fd45285c6beb18790f1ae4b189e
Patrick Harbers <jgrharbers@gmail.com>
2017-01-13 14:19:56 +0100

    Update dalvik heap values to Intel standards
    Change-Id: Ia6dd0a9d0707c70aa0530a96f90e4d1dcaebf989

---
+++ configs/phone-hdpi-1024-dalvik-heap.mk
@@ -0,0 +1,8 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=2m \
+    dalvik.jit.code_cache_size=1048576
