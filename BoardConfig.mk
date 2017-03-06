8f8c2fb13120f9e16646b54e5a7150fe339eb9a9
Patrick Harbers <jgrharbers@gmail.com>
2017-03-06 01:37:36 +0100

    Use the same defconfig name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,7 @@ RAZRI_IMAGE := true
 # Kernel build (source:github.com/oxavelar)
 BOARD_KERNEL_BASE := 0x000400
 BOARD_KERNEL_PAGESIZE := 4096
-TARGET_KERNEL_CONFIG := i386_mfld_moto_defconfig
+TARGET_KERNEL_CONFIG := i386_mfld_hazou_defconfig
 BOARD_KERNEL_IMAGE_NAME := bzImage
 KERNEL_TOOLCHAIN_PREFIX := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/x86/i686-linux-android-4.7/bin/i686-linux-android-
 BOARD_KERNEL_CMDLINE := init=/init pci=noearly console=logk0 vmalloc=260046848 earlyprintk=nologger
