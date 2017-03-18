f9b29284e4f00543d708c5841afb78b82a648cd9
Patrick Harbers <jgrharbers@gmail.com>
2017-03-18 13:13:17 +0100

    Adjust memory settings for 1G devices

--- configs/phone-hdpi-1024-dalvik-heap.mk
+++ configs/phone-hdpi-1024-dalvik-heap.mk
@@ -1,7 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heapsize=256m \
+    dalvik.vm.heapsize=174m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=2m
