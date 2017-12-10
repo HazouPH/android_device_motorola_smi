683e5f7b0b06ad0207c6982de679c52e49d3fb17
Patrick Harbers <jgrharbers@gmail.com>
2017-12-10 14:30:58 +0100

    Revert "bluetooth: Our kernel is missing CLOCK_BOOTTIME_ALARM"
    This reverts commit 02c259afecfdd95a811316d9d1301d55a16ceab8.
    
    Change-Id: Id5b54d9f4a3eea35237e8a49f67b35062c8a50cc

--- configs/bluetooth/bdroid_buildcfg.h
+++ configs/bluetooth/bdroid_buildcfg.h
@@ -24,7 +24,4 @@
 #define BLE_INCLUDED TRUE
 #define BTA_GATT_INCLUDED TRUE
 
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
-
 #endif
